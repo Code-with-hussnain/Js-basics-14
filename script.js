0338c5974b73cf6d32ca8e7aabb495435eed56d6
Muhammad Hussnain <muhammadhussnain326@gmail.com>
2025-10-26 14:15:05 +0500

    Events code

---
+++ script.js
@@ -0,0 +1,5 @@
+let button = document.getElementById("btn");
+button.addEventListener("click", ()=>{
+    alert("I was clicked")
+    document.querySelector(".box").innerHTML = "<b> Yayy You were clicked! </b> Enjoy your click "
+})
